42d736660f388bc00fc7661e4cdf8085a11e74f6
Amal <Amaladm2016@gmail.com>
2024-06-07 14:17:06 +0300

    complate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Menu';
+total =0;
+Quan=0;
+  foodmenu=[
+    {id:1,name:"دجاج",price:30,description:"دجاج مشوي على الفحم",img:"/assets/m.jpeg"},
+    {id:2,name:"دجاج",price:30,description:"دجاج شواية ",img:"/assets/v.jpeg"},
+    {id:3,name:"ايدام",price:20,description:"ايدام خضار مشكل",img:"/assets/m.jpeg"},
+    {id:4,name:"ايدام",price:20,description:"ايدام  بالدجاج على الفرن",img:"/assets/m.jpeg"}
+  ]
+event: any;
+
+
+  sum(event:any,price:number){
+    const ischecked:boolean =event.target.checked;
+    console.log(event,price)
+    if(!ischecked){
+      console.log("You hadn't select any thing! ")
+    }
+    if(ischecked){
+      this.total=this.total+ price;
+      this.Quan+=1;
+    }
+    else{
+      this.total=this.total-price;
+      this.Quan-=1;
+    }
+
+  }
 }
